b37a38a955e068d4eb83ed6371188098220e2c44
Semal Patel <semalPatel@users.noreply.github.com>
2026-03-22 16:27:41 -0700

    feat: add app shell nav host and hilt wiring

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,26 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.9.1"
+        id("org.jetbrains.kotlin.android") version "2.2.21"
+        id("org.jetbrains.kotlin.kapt") version "2.2.21"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.2.21"
+        id("com.google.dagger.hilt.android") version "2.57.1"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "vodr-android"
 
 include(
